46d874d1f802433048f2b3f8ca59e62fa9c2e37f
Ala Eddine Khiari <135550744+AlaEddine-Khiari@users.noreply.github.com>
2024-02-29 13:50:58 +0100

    Update test_script.py

--- Test/test_script.py
+++ Test/test_script.py
@@ -1,7 +1,6 @@
 import unittest
 from unittest.mock import patch, mock_open
 from script import fetch_internals_user, update_sip_conf
-import os
 
 class TestScript(unittest.TestCase):
 
@@ -16,24 +15,23 @@ class TestScript(unittest.TestCase):
 
     @patch('builtins.open', new_callable=mock_open)
     @patch('builtins.print')
-    def test_update_sip_conf(self, mock_print, mock_open):
+    def test_update_sip_conf(self, mock_print, mock_open_write):
         # Mock to simulate reading file content
         mock_open_read = mock_open(read_data="[internals]\n[existing_user]\n").return_value
         
-        # Mock to assert file opening behavior in update_sip_conf
-        mock_open_write = mock_open()
+        mock_open_write.side_effect = [mock_open_read, FileNotFoundError]
         
-        mock_open.side_effect = [mock_open_read, FileNotFoundError]
-        
-        with patch('builtins.open', mock_open_write):
-            update_sip_conf('/Test/sip.conf')  # Assuming the file is in the Test directory
+        update_sip_conf('/Test/sip.conf')  # Assuming the file is in the Test directory
 
         # Check if the file is opened with the correct mode ('w' for write)
         mock_open_write.assert_called_once_with('/Test/sip.conf', 'w')
 
         # Check if the required lines are added to the file
-        mock_open_write.return_value.write.assert_any_call("[internals]\n")
-        mock_open_write.return_value.write.assert_any_call("[existing_user]\n")
+        expected_lines = [
+            "[internals]\n",
+            "[existing_user]\n"
+        ]
+        mock_open_write.return_value.writelines.assert_called_once_with(expected_lines)
 
         # Check if the success message is printed
         mock_print.assert_called_with("sip.conf updated successfully")
